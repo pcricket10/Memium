f534c695dd2689eb356ea54d063b498ab5338c1c
Philip McCrickard <pcricket10@gmail.com>
2022-03-14 18:37:16 -0400

    created models for comment, like, meme, and user

---
+++ db/migrations/20220314223625-create-like.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      users_id: {
+        type: Sequelize.INTEGER
+      },
+      meme_id: {
+        type: Sequelize.INTEGER
+      },
+      liked: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Likes');
+  }
+};
