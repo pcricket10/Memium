37061e0ed8aeb73cdf80801639a2f331a628c848
Alex Min <alexmin@2424@yahoo.com>
2022-03-15 13:51:15 -0700

    create-memepugstart

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
-// const memeRouter = require('./routes/meme');
+const memeRouter = require('./routes/meme');
+
+
 
 
 
@@ -41,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-// app.use('/meme', memeRouter);
+app.use('/meme', memeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/meme.js
+++ routes/meme.js
@@ -1,1 +1,12 @@
 const { csrfProtection, asyncHandler } = require('./utils');
+
+const express = require('express');
+const router = express.Router();
+const { Meme } = require('../db/models');
+
+/* GET meme page. */
+router.get('/create', csrfProtection, function(req, res, next) {
+  res.render('create-meme', { title: 'Memeium - Create Meme' });
+});
+
+module.exports = router;
