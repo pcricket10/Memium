931f8d748daf764816e27de76f4798d6047f5f73
Philip McCrickard <pcricket10@gmail.com>
2022-03-19 16:00:47 -0400

    cleaned up unused code

--- routes/like.js
+++ routes/like.js
@@ -6,49 +6,6 @@ const router = express.Router();
 const db = require('../db/models');
 const { requireAuth } = require('../auth');
 
-// router.post('/meme/:id(\\d+/like/add)', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-//     const { userId } = req.session.auth;
-//     const  memeId  = parseInt(req.params.id);
-
-//     const meme = await db.Meme.findByPk(memeId { include: })
-
-//     const like = await db.Like.build({
-//         userId,
-//         memeId,
-//     })
-//     const { name,
-//       meme_url
-//     } = req.body
-
-//     const meme = db.Meme.build({
-//       name,
-//       meme_url,
-//       user_id: userId
-//     })
-//     const validatorErrors = validationResult(req);
-
-//     if (validatorErrors.isEmpty()) {
-//       await meme.save();
-//       res.redirect('/');
-//     } else {
-//       const errors = validatorErrors.array().map((error) => error.msg);
-//       res.render('create-meme', {
-//         title: 'Create Meme',
-//         meme,
-//         errors,
-//         csrfToken: req.csrfToken(),
-//       });
-//     }
-//   }));
-// router.patch('/meme/:id(\\d+)', asyncHandler(async (req, res) => {
-//     console.log('like-----------------!!!!!!!!!!!!!!!!!!')
-//     const { userId } = req.session.auth;
-//     const memeId = parseInt(req.params.id);
-//     // const { liked } = req.body
-//     // const url = window.location.href;
-//     // const strs = url.split('/');
-//     // const memeId = strs.at(-1)
-//     console.log(memeId)
 
 
 
@@ -56,22 +13,7 @@ const { requireAuth } = require('../auth');
 
 
 
-//     // console.log(memeId);
 
-//     // const meme = db.liked.build({
-//     //     user_id: userId,
-//     //     memeId: memeId,
-//     //     liked: liked
-//     // });
-
-//     // await meme.save();
-//     // if (meme) {
-//     //     res.json({ msg: "Meme liked!!!" });
-//     // } else {
-//     //     res.json({ msg: "Meh Meme" });
-//     // }
-
-// }));
 
 router.post('/meme/:id(\\d+)', asyncHandler(async (req, res) => {
     //finds current user and meme id
@@ -116,13 +58,6 @@ router.post('/meme/:id(\\d+)', asyncHandler(async (req, res) => {
         }
     }
 
-
-
-    // // const { liked } = req.body
-    // // const url = window.location.href;
-    // // const strs = url.split('/');
-    // // const memeId = strs.at(-1)
-    // console.log(meme_id)
 
 
 
