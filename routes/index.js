f7ce36721c95fa4e66739c9bf29ca8c9fbc96b2b
Philip McCrickard <pcricket10@gmail.com>
2022-12-06 14:52:23 -0500

    blah

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const { Meme } = require('../db/models');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const memes = await Meme.findAll()
-  console.log(req.session, "!!!!!")
 
   // res.render('index', { name: 'a/A Express Skeleton Home', url: 'https://images.theconversation.com/files/177834/original/file-20170712-14488-19lw3sc.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip' });
   res.render('index', { memes })
